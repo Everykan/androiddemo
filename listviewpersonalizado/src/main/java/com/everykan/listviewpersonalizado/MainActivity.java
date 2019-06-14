0d55263756919fb7ae30eeb0f1433af19e299977
Everykan <fjimenez_bcn@hotmail.com>
2019-06-14 12:53:12 +0200

    nuevo proyecto lista con imagen

---
+++ listviewpersonalizado/src/main/java/com/everykan/listviewpersonalizado/MainActivity.java
@@ -0,0 +1,40 @@
+package com.everykan.listviewpersonalizado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String [][] datos = {
+
+            {"Chet Baker","Jazz","2:49","9","Descripcion del album del artista"},
+            {"Paco Rivera","Latin-Jazz","3:10","10","Descripcion del album del artista"},
+            {"Lucia Mendez","Jazz","3:10","7","Descripcion del album del artista"},
+            {"Annabel Duran","Bosanova","2:49","8","Descripcion del album del artista"},
+            {"Chet Baker","Clasica","4:15","6","Descripcion del album del artista"},
+            {"Pedro Pérez","Pop","2:49","5","Descripcion del album del artista"},
+            {"Miguel García","Jazz","2:49","8","Descripcion del album del artista"},
+            {"Luis Enrique","Salsa","2:49","9","Descripcion del album del artista"},
+
+    };
+
+    private int[] datosImg = {
+
+            R.drawable.imagen1;
+            R.drawable.imagen2;
+            R.drawable.imagen3;
+            R.drawable.imagen4;
+            R.drawable.imagen5;
+            R.drawable.imagen6;
+            R.drawable.imagen7;
+            R.drawable.imagen8;
+
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- simplelistview/src/main/java/com/everykan/simplelistview/MainActivity.java
+++ simplelistview/src/main/java/com/everykan/simplelistview/MainActivity.java
@@ -2,35 +2,55 @@ package com.everykan.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
-    String[] valores = {"Pepín","Verónica","José","Luis","Lucia","Manuel","Jordi","Pepe","Amanda","María","Alexander","Gabriela","Sergi","Melixza","Gabriela","Tatiana","Marcel"};
-
-
+    private ListView listView;
+    private String[] valores = {"Pepín","Verónica","José","Luis","Lucia","Manuel","Jordi","Pepe","Amanda","María","Alexander","Gabriela","Sergi","Melixza","Gabriela","Tatiana","Marcel"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listView = (ListView) findViewById(R.id.idListView);
 
         // necesitamos un adaptador
 
-        // pasaremos 3 parámetros al construtor son:
+        // le pasaremos 3 parámetros al constructor son:
 
         // 1.- contexto
         // 2.- tipo de gráfico o como se va a ver
         // 3.- el array de Strings...
         // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, valores);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1, valores);
 
         listView.setAdapter(adapter);
 
+        // vamos a añadir un listener!
+        // es el que esta pendiente cuando ocurre algún tipo de evento en la app; ejemplo un clic
+        // y saber en donde o quien
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String texto = "position: " + position + "de Nombre: " + valores[position];
+                texto += " id: " + id + " view " + view.getClass().getSimpleName();
+                texto += " parent: " + parent.getClass().getSimpleName();
+
+                Toast.makeText(getApplicationContext(),texto, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
